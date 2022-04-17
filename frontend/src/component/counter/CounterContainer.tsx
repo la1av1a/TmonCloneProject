3a6776f3593b479e8f131ed128fd45c05992ca9a
jungmin.sung <ivsinji746@naver.com>
2022-04-17 20:59:17 +0900

    redux boiler plate add thunk need to add

---
+++ frontend/src/component/counter/CounterContainer.tsx
@@ -0,0 +1,28 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { countSelector, increase, decrease } from 'redux/counter'
+import Counter from "./Counter";
+
+const CounterContainer = (): JSX.Element => {
+    const currentCount = useSelector(countSelector)
+    const dispathcer = useDispatch(); 
+    const handleIncrement = useCallback(() => {
+        dispathcer(increase())
+    }, []);
+
+    const handleDecrement = useCallback(() => {
+        dispathcer(decrease())
+    }, []);
+
+
+    return (
+        <Counter 
+                value={currentCount}
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+            />
+    );
+  }
+  
+  export default CounterContainer;
+  
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './component/App'
+import App from 'component/App'
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from 'redux/rootStore';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
@@ -16,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
